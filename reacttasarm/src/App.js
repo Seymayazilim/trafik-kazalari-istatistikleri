8f610a61336cf273bbce54bac9ef189043050113
Seymayazilim <115348881+Seymayazilim@users.noreply.github.com>
2023-12-21 12:05:28 +0300

    TRAFIK

---
+++ reacttasarm/src/App.js
@@ -0,0 +1,39 @@
+import User from './components/User';
+
+
+
+import './App.css';
+
+
+
+
+
+
+
+
+
+function App() {
+
+
+
+  return (
+    <div className='bacg'>
+
+
+
+
+
+
+
+
+  
+<User/>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
